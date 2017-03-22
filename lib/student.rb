5bda5d2b7b4f020f293806b35d7dda09ee00d699
github username <github email address>
2017-03-22 17:04:24 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,18 +3,78 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+  student = Student.new
+  student.id = row[0]
+  student.name = row[1]
+  student.grade = row[2]
+  student 
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+  sql = <<-SQL
+        SELECT * FROM students;
+        SQL
+  results = DB[:conn].execute(sql)
+  results.collect do |row|
+    self.new_from_db(row)
+  end
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+            SELECT * FROM students WHERE name = ?
+            SQL
+      
+    results = DB[:conn].execute(sql, name)
+    
+    results.collect do |row|
+    self.new_from_db(row)
+    end.first
   end
   
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+          SELECT *, COUNT(grade) FROM students WHERE grade = 9;
+          SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+          SELECT * FROM students WHERE grade < 12;
+          SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.first_x_students_in_grade_10(num)
+    sql = <<-SQL
+          SELECT * FROM students WHERE grade = 10 LIMIT ?;
+          SQL
+    DB[:conn].execute(sql, num)
+  end
+
+  def self.first_student_in_grade_10
+     sql = <<-SQL
+          SELECT * FROM students WHERE grade = 10 LIMIT 1;
+          SQL
+    results = DB[:conn].execute(sql)
+    results.collect do |row|
+    self.new_from_db(row)
+    end.first
+  end
+
+  def self.all_students_in_grade_x(num)
+    sql = <<-SQL
+        SELECT * FROM students WHERE grade = ?;
+        SQL
+    DB[:conn].execute(sql, num)
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
